1fc18256221c03e9f4392c7255728be17bf89da9
Richa Patel <rpatel4401@conestogac.on.ca>
2019-05-30 00:45:00 -0400

    Complete Assignment

--- RichaAssignment1/Program.cs
+++ RichaAssignment1/Program.cs
@@ -20,7 +20,7 @@ namespace RichaAssignment1
         public void Fun()
         {
             Rectangle rectangle = new Rectangle();
-
+            
             string r_length = "";
             int length;
 
@@ -30,6 +30,7 @@ namespace RichaAssignment1
             Console.WriteLine("length must be greater than 0.");
             try
             {
+                //LOOP CAN ASK USER WHETHER COUNTINUE PROGRAM OR EXIST
                 while (true)
                 {
                     Console.WriteLine("\n1.Get Rectangle Length");
@@ -43,22 +44,27 @@ namespace RichaAssignment1
                     String choice = "";
                     int ch;
 
+                    //ASK USER ABOUT LENGTH AND WIDTH OF RECTANGLE
                     Console.WriteLine("\nplease enter number :");
                     choice = Console.ReadLine();
                     ch = int.Parse(choice);
 
+                    //LOOP BREAK IF USER WANTS TO EXIST
                     if (ch == 7)
                     {
                         Console.WriteLine("thank you for choosing our application");
                         break;
                     }
+                    //LOOP COUNTINUES RUNNING WHILE USRE GIVE CHOICE BETWEEN 1 TO 6
                     else
                     {
                         switch (ch)
                         {
+                            // CASE 1 FOR GET RECTANGLE LENGTH
                             case 1:
-                                Console.WriteLine("\nThe Length of Rectangle is {0}", rectangle.GetLength());
+                                Console.WriteLine("\nThe Length of Rectangle is {0} X {1}", rectangle.GetLength(), rectangle.GetWidth());
                                 break;
+                            // CASE 2 FOR CHANGE RECTANGLE LENGTH
                             case 2:
                                 do
                                 {
@@ -72,13 +78,15 @@ namespace RichaAssignment1
                                     }
                                 } while (!int.TryParse(r_length, out length) || (length <= 1));
                                 rectangle.SetLength(length);
-                                Console.WriteLine("\nThe Length of Rectangle is {0}", rectangle.GetLength());
+                                Console.WriteLine("\nThe changed Length of Rectangle is {0} X {1}", rectangle.GetLength(), rectangle.GetWidth());
                                 break;
 
+                            // CASE 3 FOR GET RECTANGLE WIDTH
                             case 3:
-                                Console.WriteLine("\nThe Width of Rectangle is {0}", rectangle.GetWidth());
-                                break;
+                                Console.WriteLine("\nThe Width of Rectangle is {0} x {1}", rectangle.GetLength(),rectangle.GetWidth());
+                                break;                               
 
+                            // CASE 4 FOR CHANGE RECTANGLE WIDTH
                             case 4:
                                 do
                                 {
@@ -93,11 +101,15 @@ namespace RichaAssignment1
                                 } while (!int.TryParse(r_width, out width) || (width <= 1));
 
                                 rectangle.SetWidth(width);
-                                Console.WriteLine("the width of rectangle {0}", rectangle.GetWidth());
+                                Console.WriteLine("the changed width of rectangle {0} X {1}", rectangle.GetLength(),rectangle.GetWidth());
                                 break;
+
+                            // CASE 5 FOR GET RECTANGLE PERIMETER
                             case 5:
                                 Console.WriteLine("the perimeter of rectangle is :{0}", rectangle.GetPerimeter());
                                 break;
+
+                            // CASE 6 FOR GET RECTANGLE AREA
                             case 6:
                                 Console.WriteLine("the perimeter of rectangle is :{0}", rectangle.GetArea());
                                 break;
--- RichaAssignment1/Rectangle.cs
+++ RichaAssignment1/Rectangle.cs
@@ -11,47 +11,52 @@ namespace RichaAssignment1
         private int length;
         private int width;
 
+        // DEFAULT CONSTRUCTOR SET BOTH VALUE BY 1
         public Rectangle()
         {
-
             length = 1;
             width = 1;
-
         }
-
+       
         public Rectangle(int length, int width)
         {
             this.length = length;
             this.width = width;
         }
 
+        // GET LENGTH OF RECTANGLE
         public int GetLength()
         {
             return this.length;
         }
 
+        // SET LENGTH OF RECTANGLE
         public int SetLength(int length)
         {
             this.length = length;
             return this.length;
         }
 
+        // GET WIDTH OF RECTANGLE
         public int GetWidth()
         {
             return this.width;
         }
 
+        // SET WIDTH OF RECTANGLE
         public int SetWidth(int width)
         {
             this.width = width;
             return this.width;
         }
 
+        //GET PARAMETER VALUE OF RECTANGLE
         public int GetPerimeter()
         {
             return 2 * (length + width);
         }
 
+        //GET AREA OF RECTANGLE
         public int GetArea()
         {
             return  length* width;
